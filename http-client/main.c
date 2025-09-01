02e6a4891881d59858d50525d8c4b55de69872d4
astyrisk <marguesto@disroot.org>
2025-09-01 03:27:02 +0300

    Add Template: http-client

---
+++ http-client/main.c
@@ -0,0 +1,13 @@
+/*
+ *
+ * http-client: A simple program that connects to a website, requests a page, and prints the result.
+ * with HTTPS Support (OpenSSL)
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+
+}
